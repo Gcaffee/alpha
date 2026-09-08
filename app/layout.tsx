8eaeb7c2d890de0de802910ef04b22111a179962
Gcaffee <gcaffe.shashank@gmail.com>
2026-09-08 09:06:57 +0530

    updated page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Alpha Aircraft Systems &#8211; APU Overhauling Expert for aircraft APU repair, maintenance, fuel systems, hydraulic, plasma spray and professional welding",
+  title: "Alpha Aircraft Systems, APU Overhauling Expert for aircraft APU repair, maintenance, fuel systems, hydraulic, plasma spray and professional welding",
   description:
     "Strategic design partner to bold digital brands.",
   icons: {
